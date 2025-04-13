d8ce261c2df1738f4e0f0ce66897918b1fffd8b5
Pedro AZEVEDO <azevedo.pedro176@gmail.com>
2025-04-13 19:26:55 +0200

    Update Test_AssetNonTrader.py

--- strategies/Test_AssetNonTrader.py
+++ strategies/Test_AssetNonTrader.py
@@ -1,229 +1,83 @@
-import json
-from abc import abstractmethod
-from collections import deque
-from datamodel import Listing, Observation, Order, OrderDepth, ProsperityEncoder, Symbol, Trade, TradingState
-from typing import Any, TypeAlias
+from typing import List
+import jsonpickle
+from datamodel import OrderDepth, UserId, TradingState, Order, ConversionObservation
+import numpy as np
+import math
 
-JSON: TypeAlias = dict[str, "JSON"] | list["JSON"] | str | int | float | bool | None
 
-class Logger:
-    def __init__(self) -> None:
-        self.logs = ""
-        self.max_log_length = 3750
+class Product:
 
-    def print(self, *objects: Any, sep: str = " ", end: str = "\n") -> None:
-        self.logs += sep.join(map(str, objects)) + end
 
-    def flush(self, state: TradingState, orders: dict[Symbol, list[Order]], conversions: int, trader_data: str) -> None:
-        base_length = len(self.to_json([
-            self.compress_state(state, ""),
-            self.compress_orders(orders),
-            conversions,
-            "",
-            "",
-        ]))
+    RAINFOREST_RESIN = "RAINFOREST_RESIN"
+    KELP = "KELP" 
+    SQUID_INK = "SQUID_INK" 
+    PICNIC_BASKET1 = "PICNIC_BASKET1" 
+    PICNIC_BASKET2 = "PICNIC_BASKET2"
+    CROISSANTS = "CROISSANTS" 
+    JAMS = "JAMS"
 
-        max_item_length = (self.max_log_length - base_length) // 3
 
-        print(self.to_json([
-            self.compress_state(state, self.truncate(state.traderData, max_item_length)),
-            self.compress_orders(orders),
-            conversions,
-            self.truncate(trader_data, max_item_length),
-            self.truncate(self.logs, max_item_length),
-        ]))
+PARAMS = {Product.GIFT_BASKET: {"min_width": 1, "max_width": 8, "mm_min_volume": 10}}
 
-        self.logs = ""
-
-    def compress_state(self, state: TradingState, trader_data: str) -> list[Any]:
-        return [
-            state.timestamp,
-            trader_data,
-            self.compress_listings(state.listings),
-            self.compress_order_depths(state.order_depths),
-            self.compress_trades(state.own_trades),
-            self.compress_trades(state.market_trades),
-            state.position,
-            self.compress_observations(state.observations),
-        ]
-
-    def compress_listings(self, listings: dict[Symbol, Listing]) -> list[list[Any]]:
-        return [[listing.symbol, listing.product, listing.denomination] for listing in listings.values()]
-
-    def compress_order_depths(self, order_depths: dict[Symbol, OrderDepth]) -> dict[Symbol, list[Any]]:
-        return {symbol: [depth.buy_orders, depth.sell_orders] for symbol, depth in order_depths.items()}
-
-    def compress_trades(self, trades: dict[Symbol, list[Trade]]) -> list[list[Any]]:
-        return [[
-            trade.symbol, trade.price, trade.quantity,
-            trade.buyer, trade.seller, trade.timestamp
-        ] for arr in trades.values() for trade in arr]
-
-    def compress_observations(self, observations: Observation) -> list[Any]:
-        return [
-            observations.plainValueObservations,
-            {
-                product: [
-                    obs.bidPrice, obs.askPrice, obs.transportFees,
-                    obs.exportTariff, obs.importTariff, obs.sunlight, obs.humidity
-                ] for product, obs in observations.conversionObservations.items()
-            }
-        ]
-
-    def compress_orders(self, orders: dict[Symbol, list[Order]]) -> list[list[Any]]:
-        return [[o.symbol, o.price, o.quantity] for arr in orders.values() for o in arr]
-
-    def to_json(self, value: Any) -> str:
-        return json.dumps(value, cls=ProsperityEncoder, separators=(",", ":"))
-
-    def truncate(self, value: str, max_length: int) -> str:
-        return value if len(value) <= max_length else value[:max_length - 3] + "..."
-
-logger = Logger()
-
-class Strategy:
-    def __init__(self, symbol: str, limit: int) -> None:
-        self.symbol = symbol
-        self.limit = limit
-
-    @abstractmethod
-    def act(self, state: TradingState) -> None:
-        raise NotImplementedError()
-
-    def run(self, state: TradingState) -> tuple[list[Order], int]:
-        self.orders = []
-        self.conversions = 0
-        self.act(state)
-        return self.orders, self.conversions
-
-    def buy(self, price: int, quantity: int) -> None:
-        self.orders.append(Order(self.symbol, price, quantity))
-
-    def sell(self, price: int, quantity: int) -> None:
-        self.orders.append(Order(self.symbol, price, -quantity))
-
-    def convert(self, amount: int) -> None:
-        self.conversions += amount
-
-    def save(self) -> JSON:
-        return None
-
-    def load(self, data: JSON) -> None:
-        pass
-
-class MarketMakingStrategy(Strategy):
-    def __init__(self, symbol: Symbol, limit: int) -> None:
-        super().__init__(symbol, limit)
-        self.window = deque()
-        self.window_size = 10
-
-    @abstractmethod
-    def get_true_value(self, state: TradingState) -> int:
-        raise NotImplementedError()
-
-    def act(self, state: TradingState) -> None:
-        true_value = self.get_true_value(state)
-        order_depth = state.order_depths[self.symbol]
-        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
-        sell_orders = sorted(order_depth.sell_orders.items())
-        position = state.position.get(self.symbol, 0)
-        to_buy = self.limit - position
-        to_sell = self.limit + position
-
-        self.window.append(abs(position) == self.limit)
-        if len(self.window) > self.window_size:
-            self.window.popleft()
-
-        soft_liquidate = len(self.window) == self.window_size and sum(self.window) >= self.window_size / 2 and self.window[-1]
-        hard_liquidate = len(self.window) == self.window_size and all(self.window)
-
-        max_buy_price = true_value - 1 if position > self.limit * 0.5 else true_value
-        min_sell_price = true_value + 1 if position < self.limit * -0.5 else true_value
-
-        for price, volume in sell_orders:
-            if to_buy > 0 and price <= max_buy_price:
-                quantity = min(to_buy, -volume)
-                self.buy(price, quantity)
-                to_buy -= quantity
-
-        if to_buy > 0 and hard_liquidate:
-            self.buy(true_value, to_buy // 2)
-        elif to_buy > 0 and soft_liquidate:
-            self.buy(true_value - 2, to_buy // 2)
-        elif to_buy > 0:
-            price = min(max_buy_price, max(buy_orders, key=lambda x: x[1])[0] + 1)
-            self.buy(price, to_buy)
-
-        for price, volume in buy_orders:
-            if to_sell > 0 and price >= min_sell_price:
-                quantity = min(to_sell, volume)
-                self.sell(price, quantity)
-                to_sell -= quantity
-
-        if to_sell > 0 and hard_liquidate:
-            self.sell(true_value, to_sell // 2)
-        elif to_sell > 0 and soft_liquidate:
-            self.sell(true_value + 2, to_sell // 2)
-        elif to_sell > 0:
-            price = max(min_sell_price, min(sell_orders, key=lambda x: x[1])[0] - 1)
-            self.sell(price, to_sell)
-
-    def save(self) -> JSON:
-        return list(self.window)
-
-    def load(self, data: JSON) -> None:
-        self.window = deque(data)
-
-class RainforestResinStrategy(MarketMakingStrategy):
-    def get_true_value(self, state: TradingState) -> int:
-        return 10_000
-
-class KelpStrategy(MarketMakingStrategy):
-    def get_true_value(self, state: TradingState) -> int:
-        order_depth = state.order_depths[self.symbol]
-        buy_orders = sorted(order_depth.buy_orders.items(), reverse=True)
-        sell_orders = sorted(order_depth.sell_orders.items())
-        return round((max(buy_orders, key=lambda x: x[1])[0] + min(sell_orders, key=lambda x: x[1])[0]) / 2)
-
-class SquidInkStrategy(Strategy):
-    def act(self, state: TradingState) -> None:
-        position = state.position.get(self.symbol, 0)
-        self.convert(-position)
-        obs = state.observations.conversionObservations.get(self.symbol)
-        if obs is None:
-            return
-        buy_price = obs.askPrice + obs.transportFees + obs.importTariff
-        self.sell(max(int(obs.bidPrice - 0.5), int(buy_price + 1)), self.limit)
 
 class Trader:
-    def __init__(self) -> None:
-        limits = {
-            "RAINFOREST_RESIN": 20,
-            "KELP": 20,
-            "SQUID_INK": 100,
-        }
+    def __init__(self, params=None):
+        if params is None:
+            params = PARAMS
+        self.params = params
 
-        self.strategies: dict[Symbol, Strategy] = {symbol: clazz(symbol, limits[symbol]) for symbol, clazz in {
-            "RAINFOREST_RESIN": RainforestResinStrategy,
-            "KELP": KelpStrategy,
-            "SQUID_INK": SquidInkStrategy,
-        }.items()}
+        self.LIMIT = {Product.GIFT_BASKET: 60}
 
-    def run(self, state: TradingState) -> tuple[dict[Symbol, list[Order]], int, str]:
-        orders = {}
+    def run(self, state: TradingState):
+        traderObject = {}
+        if state.traderData != None and state.traderData != "":
+            traderObject = jsonpickle.decode(state.traderData)
+
+        result = {}
         conversions = 0
-        old_trader_data = json.loads(state.traderData) if state.traderData != "" else {}
-        new_trader_data = {}
 
-        for symbol, strategy in self.strategies.items():
-            if symbol in old_trader_data:
-                strategy.load(old_trader_data[symbol])
-            if symbol in state.order_depths:
-                strategy_orders, strategy_conversions = strategy.run(state)
-                orders[symbol] = strategy_orders
-                conversions += strategy_conversions
-            new_trader_data[symbol] = strategy.save()
+        if (
+            Product.GIFT_BASKET in self.params
+            and Product.GIFT_BASKET in state.order_depths
+        ):
+            basket_params = self.params[Product.GIFT_BASKET]
+            basket_limit = self.LIMIT[Product.GIFT_BASKET]
 
-        trader_data = json.dumps(new_trader_data, separators=(",", ":"))
-        logger.flush(state, orders, conversions, trader_data)
-        return orders, conversions, trader_data
+            basket_position = (
+                state.position[Product.GIFT_BASKET]
+                if Product.GIFT_BASKET in state.position
+                else 0
+            )
+
+            basket_order_depth = state.order_depths[Product.GIFT_BASKET]
+
+            mm_bids = [
+                level
+                for level in basket_order_depth.buy_orders.keys()
+                if abs(basket_order_depth.buy_orders[level]) >= basket_params["mm_min_volume"]
+            ]
+            mm_asks = [
+                level
+                for level in basket_order_depth.sell_orders.keys()
+                if abs(basket_order_depth.sell_orders[level]) >= basket_params["mm_min_volume"]
+            ]
+            if len(mm_bids) > 0 and len(mm_asks) > 0:
+                best_mm_bid = max(mm_bids)
+                best_mm_ask = min(mm_asks)
+                # truncate
+                mm_mid = int((best_mm_bid + best_mm_ask)/2)
+
+                num_levels = basket_params['max_width'] - basket_params['min_width'] + 1
+                num_buy_levels = min(num_levels, basket_limit - basket_position)
+                num_sell_levels = min(num_levels, basket_limit + basket_position)
+
+                basket_orders = []
+                for level in range(1, num_buy_levels + 1):
+                    basket_orders.append(Order(Product.GIFT_BASKET, mm_mid - level, 1))
+                for level in range(1, num_sell_levels + 1):
+                    basket_orders.append(Order(Product.GIFT_BASKET, mm_mid + level, -1))
+                
+                result[Product.GIFT_BASKET] = basket_orders
+        traderData = jsonpickle.encode(traderObject)
+
+        return result, conversions, traderData
