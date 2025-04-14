3f04a00ca14602f21b88e28a0ef49033e304b47e
Pedro AZEVEDO <azevedo.pedro176@gmail.com>
2025-04-14 23:32:17 +0200

    r

--- strategies/test_opti_round3.py
+++ strategies/test_opti_round3.py
@@ -846,11 +846,11 @@ class Trader:
             "PICNIC_BASKET1": 60,
             "PICNIC_BASKET2": 100,
             "VOLCANIC_ROCK": 400,
-            "VOLCANIC_ROCK_VOUCHER_9500": 200, #Expiration 7 days (=round) for all voucher,
-            "VOLCANIC_ROCK_VOUCHER_9750": 200,
-            "VOLCANIC_ROCK_VOUCHER_10000": 200,
-            "VOLCANIC_ROCK_VOUCHER_10250": 200,
-            "VOLCANIC_ROCK_VOUCHER_10500": 200,
+            "VOLCANIC_ROCK_VOUCHER_9500": 100, #Expiration 7 days (=round) for all voucher,
+            "VOLCANIC_ROCK_VOUCHER_9750": 100,
+            "VOLCANIC_ROCK_VOUCHER_10000": 100,
+            "VOLCANIC_ROCK_VOUCHER_10250": 100,
+            "VOLCANIC_ROCK_VOUCHER_10500": 100,
         }
 
         self.strategies: dict[Symbol, Strategy] = {symbol: clazz(symbol, limits[symbol]) for symbol, clazz in {
