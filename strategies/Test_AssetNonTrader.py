50dfc316fb8a3c7a808cde3c67d5b6f0deddf0ba
Pedro AZEVEDO <azevedo.pedro176@gmail.com>
2025-04-15 23:04:43 +0200

    e

--- strategies/Test_AssetNonTrader.py
+++ strategies/Test_AssetNonTrader.py
@@ -15,7 +15,7 @@ class Product:
     ROSES = "ROSES"
 
 
-PARAMS = {Product.DJEMBES: {"min_width": 1, "max_width": 8, "mm_min_volume": 8}}
+PARAMS = {Product.DJEMBES: {"min_width": 1, "max_width": 8, "mm_min_volume": 5}}
 
 
 class Trader:
