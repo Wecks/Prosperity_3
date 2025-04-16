c82f3b7858fa91ded8d76d8182d6b173c6cfdd7c
Pedro AZEVEDO <azevedo.pedro176@gmail.com>
2025-04-17 00:00:13 +0200

    r

--- strategies/test_macarons.py
+++ strategies/test_macarons.py
@@ -223,7 +223,7 @@ class MAGNIFICENT_MACARONSStrategy(Strategy):
         if not obs:
             return
         buy_price = obs.askPrice + obs.transportFees + obs.importTariff
-        self.sell(max(int(obs.bidPrice - 0.1), int(buy_price + 0.80)), self.limit)
+        self.sell(max(int(obs.bidPrice - 0.1), int(buy_price + 0.1)), self.limit)
 
 class Trader:
     def __init__(self) -> None:
