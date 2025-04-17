aa641845034b10819a3af9f1735d507c44e7db21
Pedro AZEVEDO <azevedo.pedro176@gmail.com>
2025-04-18 00:14:01 +0200

    e

--- strategies/test_macarons2.py
+++ strategies/test_macarons2.py
@@ -28,15 +28,15 @@ PARAMS = {
         "starfruit_min_edge": 2,
     },
     Product.MAGNIFICENT_MACARONS:{
-        "make_edge": 0.2,
-        "make_min_edge": 0.3,
-        "make_probability": 0.1,
-        "init_make_edge": 0.2,
-        "min_edge": 0.2,
-        "volume_avg_timestamp": 5,
-        "volume_bar": 35,
-        "dec_edge_discount": 0.8,
-        "step_size": 0.5
+        "make_edge": 0.2,      # base value 2
+        "make_min_edge": 0.3,  # base value 1
+        "make_probability": 0.1,  # base value 0.566
+        "init_make_edge": 0.2,    # base value 2
+        "min_edge": 0.2,          # base value 0.5
+        "volume_avg_timestamp": 5,# base value 5
+        "volume_bar": 35,         # base value 75
+        "dec_edge_discount": 0.8, # base value 0.8
+        "step_size": 0.5          # base value 0.5
     }
 }
 
@@ -59,6 +59,7 @@ class Trader:
             # "optimized": False  # Désactivé
         }
 
+    # Returns buy_order_volume, sell_order_volume
     def take_best_orders(
         self,
         product: str,
@@ -75,9 +76,9 @@ class Trader:
         position_limit = self.LIMIT[product]
         if order_depth.sell_orders:
             best_ask = min(order_depth.sell_orders.keys())
-            best_ask_amount = -order_depth.sell_orders[best_ask]
+            best_ask_amt = -order_depth.sell_orders[best_ask]
             if best_ask <= fair_value - take_width:
-                qty = min(best_ask_amount, position_limit - position)
+                qty = min(best_ask_amt, position_limit - position)
                 if qty > 0:
                     orders.append(Order(product, best_ask, qty))
                     buy_order_volume += qty
@@ -87,15 +88,16 @@ class Trader:
 
         if order_depth.buy_orders:
             best_bid = max(order_depth.buy_orders.keys())
-            best_bid_amount = order_depth.buy_orders[best_bid]
+            best_bid_amt = order_depth.buy_orders[best_bid]
             if best_bid >= fair_value + take_width:
-                qty = min(best_bid_amount, position_limit + position)
+                qty = min(best_bid_amt, position_limit + position)
                 if qty > 0:
                     orders.append(Order(product, best_bid, -qty))
                     sell_order_volume += qty
                     order_depth.buy_orders[best_bid] -= qty
                     if order_depth.buy_orders[best_bid] == 0:
                         del order_depth.buy_orders[best_bid]
+
         return buy_order_volume, sell_order_volume
 
     def take_best_orders_with_adverse(
@@ -113,9 +115,9 @@ class Trader:
         position_limit = self.LIMIT[product]
         if order_depth.sell_orders:
             best_ask = min(order_depth.sell_orders.keys())
-            best_ask_amount = -order_depth.sell_orders[best_ask]
-            if abs(best_ask_amount) <= adverse_volume and best_ask <= fair_value - take_width:
-                qty = min(best_ask_amount, position_limit - position)
+            best_ask_amt = -order_depth.sell_orders[best_ask]
+            if abs(best_ask_amt) <= adverse_volume and best_ask <= fair_value - take_width:
+                qty = min(best_ask_amt, position_limit - position)
                 if qty > 0:
                     orders.append(Order(product, best_ask, qty))
                     buy_order_volume += qty
@@ -125,15 +127,16 @@ class Trader:
 
         if order_depth.buy_orders:
             best_bid = max(order_depth.buy_orders.keys())
-            best_bid_amount = order_depth.buy_orders[best_bid]
-            if abs(best_bid_amount) <= adverse_volume and best_bid >= fair_value + take_width:
-                qty = min(best_bid_amount, position_limit + position)
+            best_bid_amt = order_depth.buy_orders[best_bid]
+            if abs(best_bid_amt) <= adverse_volume and best_bid >= fair_value + take_width:
+                qty = min(best_bid_amt, position_limit + position)
                 if qty > 0:
                     orders.append(Order(product, best_bid, -qty))
                     sell_order_volume += qty
                     order_depth.buy_orders[best_bid] -= qty
                     if order_depth.buy_orders[best_bid] == 0:
                         del order_depth.buy_orders[best_bid]
+
         return buy_order_volume, sell_order_volume
 
     def market_make(
@@ -146,12 +149,14 @@ class Trader:
         buy_order_volume: int,
         sell_order_volume: int,
     ) -> (int, int):
-        buy_qty = self.LIMIT[product] - (position + buy_order_volume)
-        if buy_qty > 0:
-            orders.append(Order(product, round(bid), buy_qty))
-        sell_qty = self.LIMIT[product] + (position - sell_order_volume)
-        if sell_qty > 0:
-            orders.append(Order(product, round(ask), -sell_qty))
+        buy_quantity = self.LIMIT[product] - (position + buy_order_volume)
+        if buy_quantity > 0:
+            orders.append(Order(product, round(bid), buy_quantity))
+
+        sell_quantity = self.LIMIT[product] + (position - sell_order_volume)
+        if sell_quantity > 0:
+            orders.append(Order(product, round(ask), -sell_quantity))
+
         return buy_order_volume, sell_order_volume
 
     def clear_position_order(
@@ -165,24 +170,31 @@ class Trader:
         buy_order_volume: int,
         sell_order_volume: int,
     ) -> (int, int):
-        position_after = position + buy_order_volume - sell_order_volume
-        fair_bid = round(fair_value - width)
-        fair_ask = round(fair_value + width)
-        buy_qty = self.LIMIT[product] - (position + buy_order_volume)
-        sell_qty = self.LIMIT[product] + (position - sell_order_volume)
+        position_after_take = position + buy_order_volume - sell_order_volume
+        fair_for_bid = round(fair_value - width)
+        fair_for_ask = round(fair_value + width)
 
-        if position_after > 0:
-            clear_vol = sum(v for p, v in order_depth.buy_orders.items() if p >= fair_ask)
-            sent = min(clear_vol, position_after, sell_qty)
-            if sent > 0:
-                orders.append(Order(product, fair_ask, -abs(sent)))
-                sell_order_volume += abs(sent)
+        buy_quantity = self.LIMIT[product] - (position + buy_order_volume)
+        sell_quantity = self.LIMIT[product] + (position - sell_order_volume)
 
-        if position_after < 0:
-            clear_vol = sum(abs(v) for p, v in order_depth.sell_orders.items() if p <= fair_bid)
-            sent = min(clear_vol, abs(position_after), buy_qty)
+        if position_after_take > 0:
+            clear_quantity = sum(
+                v for p, v in order_depth.buy_orders.items() if p >= fair_for_ask
+            )
+            clear_quantity = min(clear_quantity, position_after_take)
+            sent = min(sell_quantity, clear_quantity)
             if sent > 0:
-                orders.append(Order(product, fair_bid, sent))
+                orders.append(Order(product, fair_for_ask, -sent))
+                sell_order_volume += sent
+
+        if position_after_take < 0:
+            clear_quantity = sum(
+                -v for p, v in order_depth.sell_orders.items() if p <= fair_for_bid
+            )
+            clear_quantity = min(clear_quantity, -position_after_take)
+            sent = min(buy_quantity, clear_quantity)
+            if sent > 0:
+                orders.append(Order(product, fair_for_bid, sent))
                 buy_order_volume += sent
 
         return buy_order_volume, sell_order_volume
@@ -233,17 +245,21 @@ class Trader:
         orders: List[Order] = []
         baaf = min(
             [p for p in order_depth.sell_orders.keys() if p > fair_value + 1],
-            default=fair_value+3
+            default=fair_value + 3
         )
         bbbf = max(
             [p for p in order_depth.buy_orders.keys() if p < fair_value - 1],
-            default=fair_value-3
+            default=fair_value - 3
         )
-        buy_order_volume, sell_order_volume = self.market_make(
-            Product.AMETHYSTS, orders, bbbf+1, baaf-1,
-            position, buy_order_volume, sell_order_volume
+        return self.market_make(
+            Product.AMETHYSTS,
+            orders,
+            bbbf + 1,
+            baaf - 1,
+            position,
+            buy_order_volume,
+            sell_order_volume,
         )
-        return orders, buy_order_volume, sell_order_volume
 
     def take_orders(
         self,
@@ -256,21 +272,21 @@ class Trader:
         adverse_volume: int = 0,
     ) -> (List[Order], int, int):
         orders: List[Order] = []
-        buy_order_volume, sell_order_volume = 0, 0
+        buy_vol, sell_vol = 0, 0
         if prevent_adverse:
-            buy_order_volume, sell_order_volume = self.take_best_orders_with_adverse(
-                product, fair_value, take_width, orders,
-                order_depth, position,
-                buy_order_volume, sell_order_volume,
+            buy_vol, sell_vol = self.take_best_orders_with_adverse(
+                product, fair_value, take_width,
+                orders, order_depth,
+                position, buy_vol, sell_vol,
                 adverse_volume
             )
         else:
-            buy_order_volume, sell_order_volume = self.take_best_orders(
-                product, fair_value, take_width, orders,
-                order_depth, position,
-                buy_order_volume, sell_order_volume
+            buy_vol, sell_vol = self.take_best_orders(
+                product, fair_value, take_width,
+                orders, order_depth,
+                position, buy_vol, sell_vol
             )
-        return orders, buy_order_volume, sell_order_volume
+        return orders, buy_vol, sell_vol
 
     def clear_orders(
         self,
@@ -310,11 +326,15 @@ class Trader:
         ]
         baaf = min(aaf) if aaf else round(fair_value + min_edge)
         bbbf = max(bbf) if bbf else round(fair_value - min_edge)
-        buy_order_volume, sell_order_volume = self.market_make(
-            Product.STARFRUIT, orders, bbbf+1, baaf-1,
-            position, buy_order_volume, sell_order_volume
+        return self.market_make(
+            Product.STARFRUIT,
+            orders,
+            bbbf + 1,
+            baaf - 1,
+            position,
+            buy_order_volume,
+            sell_order_volume,
         )
-        return orders, buy_order_volume, sell_order_volume
 
     def MAGNIFICENT_MACARONS_implied_bid_ask(
         self,
@@ -345,30 +365,27 @@ class Trader:
         self.MAGNIFICENT_MACARONS_data["volume_history"].append(abs(position))
         if len(self.MAGNIFICENT_MACARONS_data["volume_history"]) > self.params[Product.MAGNIFICENT_MACARONS]["volume_avg_timestamp"]:
             self.MAGNIFICENT_MACARONS_data["volume_history"].pop(0)
-
         if len(self.MAGNIFICENT_MACARONS_data["volume_history"]) < self.params[Product.MAGNIFICENT_MACARONS]["volume_avg_timestamp"]:
             return curr_edge
 
         volume_avg = np.mean(self.MAGNIFICENT_MACARONS_data["volume_history"])
-
         if volume_avg >= self.params[Product.MAGNIFICENT_MACARONS]["volume_bar"]:
             self.MAGNIFICENT_MACARONS_data["volume_history"] = []
             self.MAGNIFICENT_MACARONS_data["curr_edge"] = curr_edge + self.params[Product.MAGNIFICENT_MACARONS]["step_size"]
-            return curr_edge + self.params[Product.MAGNIFICENT_MACARONS]["step_size"]
+            return self.MAGNIFICENT_MACARONS_data["curr_edge"]
 
-        dec_cond = self.params[Product.MAGNIFICENT_MACARONS]["dec_edge_discount"] * \
-                   self.params[Product.MAGNIFICENT_MACARONS]["volume_bar"] * \
-                   (curr_edge - self.params[Product.MAGNIFICENT_MACARONS]["step_size"])
+        dec_cond = (
+            self.params[Product.MAGNIFICENT_MACARONS]["dec_edge_discount"]
+            * self.params[Product.MAGNIFICENT_MACARONS]["volume_bar"]
+            * (curr_edge - self.params[Product.MAGNIFICENT_MACARONS]["step_size"])
+        )
         if dec_cond > volume_avg * curr_edge:
-            if curr_edge - self.params[Product.MAGNIFICENT_MACARONS]["step_size"] > self.params[Product.MAGNIFICENT_MACARONS]["min_edge"]:
-                self.MAGNIFICENT_MACARONS_data["volume_history"] = []
-                self.MAGNIFICENT_MACARONS_data["curr_edge"] = curr_edge - self.params[Product.MAGNIFICENT_MACARONS]["step_size"]
-                return curr_edge - self.params[Product.MAGNIFICENT_MACARONS]["step_size"]
-            else:
-                self.MAGNIFICENT_MACARONS_data["curr_edge"] = self.params[Product.MAGNIFICENT_MACARONS]["min_edge"]
-                return self.params[Product.MAGNIFICENT_MACARONS]["min_edge"]
+            new_edge = max(curr_edge - self.params[Product.MAGNIFICENT_MACARONS]["step_size"],
+                           self.params[Product.MAGNIFICENT_MACARONS]["min_edge"])
+            self.MAGNIFICENT_MACARONS_data["volume_history"] = []
+            self.MAGNIFICENT_MACARONS_data["curr_edge"] = new_edge
+            return new_edge
 
-        self.MAGNIFICENT_MACARONS_data["curr_edge"] = curr_edge
         return curr_edge
 
     def MAGNIFICENT_MACARONS_arb_take(
@@ -379,41 +396,38 @@ class Trader:
         position: int
     ) -> (List[Order], int, int):
         orders: List[Order] = []
-        position_limit = self.LIMIT[Product.MAGNIFICENT_MACARONS]
-        buy_order_volume = 0
-        sell_order_volume = 0
-
         implied_bid, implied_ask = self.MAGNIFICENT_MACARONS_implied_bid_ask(observation)
-        buy_qty = position_limit - position
-        sell_qty = position_limit + position
+        position_limit = self.LIMIT[Product.MAGNIFICENT_MACARONS]
+        buy_vol = 0
+        sell_vol = 0
+
+        buy_cap = position_limit - position
+        sell_cap = position_limit + position
 
         ask = implied_ask + adap_edge
         foreign_mid = (observation.askPrice + observation.bidPrice) / 2
-        aggressive_ask = foreign_mid - 1
-        if aggressive_ask > implied_ask:
-            ask = aggressive_ask
+        if foreign_mid - 1 > implied_ask:
+            ask = foreign_mid - 1
 
         edge = (ask - implied_ask) * self.params[Product.MAGNIFICENT_MACARONS]["make_probability"]
 
         for price in sorted(order_depth.sell_orders.keys()):
             if price > implied_bid - edge:
                 break
-            qty = min(abs(order_depth.sell_orders[price]), buy_qty)
+            qty = min(-order_depth.sell_orders[price], buy_cap)
             if qty > 0:
-                del order_depth.sell_orders[price]
                 orders.append(Order(Product.MAGNIFICENT_MACARONS, round(price), qty))
-                buy_order_volume += qty
+                buy_vol += qty
 
         for price in sorted(order_depth.buy_orders.keys(), reverse=True):
             if price < implied_ask + edge:
                 break
-            qty = min(abs(order_depth.buy_orders[price]), sell_qty)
+            qty = min(order_depth.buy_orders[price], sell_cap)
             if qty > 0:
-                del order_depth.buy_orders[price]
                 orders.append(Order(Product.MAGNIFICENT_MACARONS, round(price), -qty))
-                sell_order_volume += qty
+                sell_vol += qty
 
-        return orders, buy_order_volume, sell_order_volume
+        return orders, buy_vol, sell_vol
 
     def MAGNIFICENT_MACARONS_arb_make(
         self,
@@ -425,31 +439,28 @@ class Trader:
         sell_order_volume: int,
     ) -> (List[Order], int, int):
         orders: List[Order] = []
-        position_limit = self.LIMIT[Product.MAGNIFICENT_MACARONS]
-
         implied_bid, implied_ask = self.MAGNIFICENT_MACARONS_implied_bid_ask(observation)
         bid = implied_bid - edge
         ask = implied_ask + edge
 
+        # if foreign mid suggests a more aggressive ask
         foreign_mid = (observation.askPrice + observation.bidPrice) / 2
-        aggressive_ask = round(observation.askPrice) - 2
-        if aggressive_ask > implied_ask:
-            ask = aggressive_ask
+        if foreign_mid - 1 > implied_ask:
+            ask = foreign_mid - 1
 
+        position_limit = self.LIMIT[Product.MAGNIFICENT_MACARONS]
         buy_qty = position_limit - (position + buy_order_volume)
         if buy_qty > 0:
             orders.append(Order(Product.MAGNIFICENT_MACARONS, round(bid), buy_qty))
 
         sell_qty = position_limit + (position - sell_order_volume)
+        # only place ask if it's above implied_ask (guaranteed profit)
         if sell_qty > 0 and ask > implied_ask:
             orders.append(Order(Product.MAGNIFICENT_MACARONS, round(ask), -sell_qty))
 
         return orders, buy_order_volume, sell_order_volume
 
-    def MAGNIFICENT_MACARONS_arb_clear(
-        self,
-        position: int
-    ) -> int:
+    def MAGNIFICENT_MACARONS_arb_clear(self, position: int) -> int:
         return -position
 
     def run(self, state: TradingState):
@@ -464,14 +475,15 @@ class Trader:
             and Product.MAGNIFICENT_MACARONS in state.observations.conversionObservations):
 
             pos = state.position.get(Product.MAGNIFICENT_MACARONS, 0)
-            conversions = 0  # pas de clear conversion
+            # conversions = self.MAGNIFICENT_MACARONS_arb_clear(pos)
+            conversions = 0
 
             obs = state.observations.conversionObservations[Product.MAGNIFICENT_MACARONS]
             edge = self.MAGNIFICENT_MACARONS_adap_edge(state.timestamp, obs, pos)
 
             od = state.order_depths[Product.MAGNIFICENT_MACARONS]
-            orders_take, buy_volume, sell_volume = self.MAGNIFICENT_MACARONS_arb_take(od, obs, edge, pos)
-            orders_make, _, _ = self.MAGNIFICENT_MACARONS_arb_make(od, obs, pos, edge, buy_volume, sell_volume)
+            orders_take, buy_v, sell_v = self.MAGNIFICENT_MACARONS_arb_take(od, obs, edge, pos)
+            orders_make, _, _  = self.MAGNIFICENT_MACARONS_arb_make(od, obs, pos, edge, buy_v, sell_v)
 
             result[Product.MAGNIFICENT_MACARONS] = orders_take + orders_make
 
