1b6912682a3ec111532936fb979ae2dbdeb83ea5
Pedro AZEVEDO <azevedo.pedro176@gmail.com>
2025-04-17 20:34:02 +0200

    e

--- strategies/optimizer_round3.py
+++ strategies/optimizer_round3.py
@@ -841,8 +841,8 @@ class PicnicBasketStrategy(SignalStrategy):
          "CROISSANTS": {"long": -100, "short": 150},
          "JAMS": {"long": 0, "short": 150},
          "DJEMBES": {"long": -30, "short": 250},
-         "PICNIC_BASKET1": {"long": -45, "short": 110},
-         "PICNIC_BASKET2": {"long": -45, "short": 120}
+         "PICNIC_BASKET1": {"long": -60, "short": 110},
+         "PICNIC_BASKET2": {"long": -60, "short": 110}
      }
 
      # Override class-level thresholds with values from command line
