4efa90ea095430b66a5046d9fa57198c2d209c2c
Pedro AZEVEDO <azevedo.pedro176@gmail.com>
2025-04-19 01:30:17 +0200

    r

--- strategies/round4.py
+++ strategies/round4.py
@@ -989,7 +989,7 @@ class MagnificentMacaronsStrategy(Strategy):
 
         pos = state.position.get(self.symbol, 0)
         self.convert(-pos)
-        self.per_trade_size = int(1 + (abs(sun - 10)/1))
+        self.per_trade_size = int(1 + (abs(sun - 0)/1))
 
 
         # 1) Persistent high sunlight: convert and sell on market
