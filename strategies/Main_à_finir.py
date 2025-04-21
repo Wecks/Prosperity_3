83357d201ddd64830fa0783fe4a8aebe3e63b089
Pedro AZEVEDO <azevedo.pedro176@gmail.com>
2025-04-21 22:39:48 +0200

    r

--- strategies/Main_à_finir.py
+++ strategies/Main_à_finir.py
@@ -748,7 +748,7 @@ class MagnificentMacaronsStrategy(Strategy):
 
 class VolcanicRockStrategy(Strategy):
     VOLCANIC_SMA_WINDOW = 50
-    VOLCANIC_THRESHOLD = 2
+    VOLCANIC_THRESHOLD = 0.01
     VOLCANIC_ORDER_SIZE = 10
     VOLCANIC_STOP_LOSS_THRESHOLD = 10000000  # effectively disables stop-loss
     PRICE_HISTORY_MAXLEN = VOLCANIC_SMA_WINDOW + 10
